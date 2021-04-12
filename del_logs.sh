f19fd79f5dde75c1123c7fc1f123196afc8f23e4
Marcel Hoffs <marcelhoffs@gmail.com>
2021-04-12 21:46:25 +0200

    updated

--- del_logs.sh
+++ del_logs.sh
@@ -19,14 +19,14 @@ echo -e "Application Server logs"
 echo -e "-----------------------"
 < domains_app sed -n 1'p' | tr ',' '\n' | while read -r app; do
   PATH_APP_LOG="$app"": ""$PS_CFG_HOME""/appserv/""$app""/LOGS"
-  echo -e "$PATH_WEB_LOG_PIA"
+  echo -e "$PATH_APP_LOG"
 done
 
 # Show Process Scheduler log paths
 echo ''
 echo -e "Process Scheduler logs"
 echo -e "----------------------"
-< domains_app sed -n 1'p' | tr ',' '\n' | while read -r app; do
+< domains_app sed -n 1'p' | tr ',' '\n' | while read -r prcs; do
   PATH_PRCS_LOG="$prcs"": ""$PS_CFG_HOME""/appserv/prcs/""$prcs""/LOGS"
   echo -e "$PATH_PRCS_LOG"
 done
