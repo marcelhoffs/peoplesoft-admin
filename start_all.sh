deca87f67f7fb5c802a806c0b3b1e646ac7668ba
Marcel Hoffs <marcelhoffs@gmail.com>
2021-04-12 21:23:08 +0200

    added comment

--- start_all.sh
+++ start_all.sh
@@ -2,10 +2,14 @@
 
 clear
 
-echo -e "Start all domains"
-echo -e "-----------------"
-echo -e "This script will start all web server, application server"
-echo -e "and process scheduler domains."
+echo -e "==========================================================="
+echo -e ""
+echo -e " Start all domains"
+echo -e " -----------------"
+echo -e " This script will start all web server, application server"
+echo -e " and process scheduler domains."
+echo -e ""
+echo -e "==========================================================="
 echo ''
 
 while [ "$CONTINUE" != 'Y' ] && [ "$CONTINUE" != 'N' ]; do
@@ -19,18 +23,20 @@ if [ "$CONTINUE" = 'Y' ]; then
   # Fetch all domains
   ./domains.sh > /dev/null 2>&1
 
-  echo ''
+  # Start Application Server domains
   echo -e ">> Starting all Application Server domains"
   < domains_app sed -n 1'p' | tr ',' '\n' | while read -r app; do
     psadmin -c start -d "$app"
   done
 
-  # Stop Web Server domains
+  # Start Web Server domains
+  echo ''
   echo -e ">> Starting all Web Server domains"
   < domains_web sed -n 1'p' | tr ',' '\n' | while read -r web; do
     psadmin -w start -d "$web"
   done
 
+  # Start Process Scheduler domains
   echo ''
   echo -e ">> Starting all Process Scheduler domains"
   < domains_prcs sed -n 1'p' | tr ',' '\n' | while read -r prcs; do
