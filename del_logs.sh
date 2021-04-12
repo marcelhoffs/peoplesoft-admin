1c5d961bfb28218ea5e5c4ad08bb73e49179a903
Marcel Hoffs <marcelhoffs@gmail.com>
2021-04-12 22:00:35 +0200

    commit

--- del_logs.sh
+++ del_logs.sh
@@ -17,7 +17,7 @@ echo -e ""
 echo -e "  Web Server logs"
 echo -e "  ---------------"
 < domains_web sed -n 1'p' | tr ',' '\n' | while read -r web; do
-  PATH_WEB_LOG="$web"": ""$PS_CFG_HOME""/webserv/""$web""/servers/PIA/logs"
+  PATH_WEB_LOG="$web"": ""$PS_CFG_HOME""/webserv/""$web""/servers/PIA/logs/*"
   echo -e "  ""$PATH_WEB_LOG"
 done
 
@@ -26,7 +26,7 @@ echo ''
 echo -e "  Application Server logs"
 echo -e "  -----------------------"
 < domains_app sed -n 1'p' | tr ',' '\n' | while read -r app; do
-  PATH_APP_LOG="$app"": ""$PS_CFG_HOME""/appserv/""$app""/LOGS"
+  PATH_APP_LOG="$app"": ""$PS_CFG_HOME""/appserv/""$app""/LOGS/*"
   echo -e "  ""$PATH_APP_LOG"
 done
 
@@ -35,7 +35,7 @@ echo ''
 echo -e "  Process Scheduler logs"
 echo -e "  ----------------------"
 < domains_prcs sed -n 1'p' | tr ',' '\n' | while read -r prcs; do
-  PATH_PRCS_LOG="$prcs"": ""$PS_CFG_HOME""/appserv/prcs/""$prcs""/LOGS"
+  PATH_PRCS_LOG="$prcs"": ""$PS_CFG_HOME""/appserv/prcs/""$prcs""/LOGS/*"
   echo -e "  ""$PATH_PRCS_LOG"
 done
 
@@ -50,11 +50,11 @@ done
 
 if [ "$CONTINUE" = 'Y' ]; then
   # Delete Web Server logs
-  rm -r "${PATH_WEB_LOG:?}""/*"
+  rm -r "$PATH_WEB_LOG"
 
   # Delete Application Server logs
-  rm -r "${PATH_APP_LOG:?}""/*"
+  rm -r "$PATH_APP_LOG"
 
   # Delete Process Scheduler logs
-  rm -r "${PATH_PRCS_LOG:?}""/*"
+  rm -r "$PATH_PRCS_LOG"
 fi
