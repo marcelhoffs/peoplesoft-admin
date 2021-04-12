6eacb31b9bccbbd1fdbb4eaf2df3f015b083d5f4
Marcel Hoffs <marcelhoffs@gmail.com>
2021-04-12 21:50:35 +0200

    commit

--- del_logs.sh
+++ del_logs.sh
@@ -14,30 +14,31 @@ echo -e " and process scheduler log files."
 echo -e ""
 
 # Show Web Server log paths
-echo -e " Web Server logs"
-echo -e " ---------------"
+echo -e "  Web Server logs"
+echo -e "  ---------------"
 < domains_web sed -n 1'p' | tr ',' '\n' | while read -r web; do
   PATH_WEB_LOG="$web"": ""$PS_CFG_HOME""/webserv/""$web""/servers/PIA/logs"
-  echo -e " ""$PATH_WEB_LOG"
+  echo -e "  ""$PATH_WEB_LOG"
 done
 
 # Show Application Server log paths
 echo ''
-echo -e " Application Server logs"
-echo -e " -----------------------"
+echo -e "  Application Server logs"
+echo -e "  -----------------------"
 < domains_app sed -n 1'p' | tr ',' '\n' | while read -r app; do
   PATH_APP_LOG="$app"": ""$PS_CFG_HOME""/appserv/""$app""/LOGS"
-  echo -e " ""$PATH_APP_LOG"
+  echo -e "  ""$PATH_APP_LOG"
 done
 
 # Show Process Scheduler log paths
 echo ''
-echo -e " Process Scheduler logs"
-echo -e " ----------------------"
+echo -e "  Process Scheduler logs"
+echo -e "  ----------------------"
 < domains_prcs sed -n 1'p' | tr ',' '\n' | while read -r prcs; do
   PATH_PRCS_LOG="$prcs"": ""$PS_CFG_HOME""/appserv/prcs/""$prcs""/LOGS"
-  echo -e " ""$PATH_PRCS_LOG"
+  echo -e "  ""$PATH_PRCS_LOG"
 done
 
+echo -e ""
 echo -e "=========================================================="
 echo ''
