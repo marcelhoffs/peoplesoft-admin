b7eb232e43d88fe81dde69a00e9d97a06bbb9d42
Marcel Hoffs <marcelhoffs@gmail.com>
2021-04-12 23:20:50 +0200

    commit

--- del_logs.sh
+++ del_logs.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# ================================================================
+# FUNCTIONS
+# ================================================================
+
 delete_web_server_logs()
 {
   arg1=$1
 
-  rm -r "${arg1:?}"/*
+  rm -r "${arg1:?}/servers/PIA/logs/"*
+  rm -r "${arg1:?}/applications/peoplesoft/PSIGW.war/WEB-INF/"*
 }
 
 delete_app_server_logs()
@@ -23,6 +28,9 @@ delete_process_scheduler_logs()
   rm -r "${arg1:?}/"ULOG.*
 }
 
+# ================================================================
+# MAIN
+# ================================================================
 clear
 
 # Fetch all domains
@@ -38,29 +46,29 @@ echo -e "║                                                            ║"
 echo -e "╚════════════════════════════════════════════════════════════╝"
 echo ''
 
-# Show Web Server log paths
+# Determine Web Server log paths
 while read -r web; do
-  PATH_WEB_LOG="${PS_CFG_HOME}/webserv/${web}/servers/PIA/logs"
+  PATH_WEB_LOG="${PS_CFG_HOME}/webserv/${web}"
 done < <(cat domains_web | sed -n 1'p' | tr ',' '\n')
 
-# Show Application Server log paths
+# Determine Application Server log paths
 while read -r app; do
   PATH_APP_LOG="${PS_CFG_HOME}/appserv/${app}"
 done < <(cat domains_app | sed -n 1'p' | tr ',' '\n')
 
-# Show Process Scheduler log paths
+# Determine Process Scheduler log paths
 while read -r prcs; do
   PATH_PRCS_LOG="${PS_CFG_HOME}/appserv/prcs/${prcs}"
 done < <(cat domains_prcs | sed -n 1'p' | tr ',' '\n')
 
-
+# Ask to continue
 while [ "$CONTINUE" != 'Y' ] && [ "$CONTINUE" != 'N' ]; do
   read -r -p 'Are you sure you want to continue? [Y/N]: ' CONTINUE
   CONTINUE=${CONTINUE^^}
 done
 
+# Delete logs
 if [ "$CONTINUE" = 'Y' ]; then
-  # Delete logs
   delete_web_server_logs "$PATH_WEB_LOG"
   delete_app_server_logs "$PATH_APP_LOG"
   delete_process_scheduler_logs "$PATH_PRCS_LOG"
