78d328be7c4a52c9d1b4aabf9f7cc898f5a25ff8
Marcel Hoffs <marcelhoffs@gmail.com>
2021-04-12 21:36:44 +0200

    commit

--- del_logs.sh
+++ del_logs.sh
@@ -5,9 +5,10 @@ clear
 # Fetch all domains
 ./domains.sh > /dev/null 2>&1
 
-# Show Application Server log paths
+# Show Web Server log paths
 < domains_web sed -n 1'p' | tr ',' '\n' | while read -r web; do
   echo -e "Application Server logs"
   echo -e "-----------------------"
-  echo -e "$web"": " $PS_CFG_HOME
+  PATH_WEB_LOG_PIA="$web"": ""$PS_CFG_HOME""/webserv/""$web""/servers/PIA/logs"
+  echo -e "$PATH_WEB_LOG_PIA"
 done
