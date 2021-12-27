a5065694cf0b96bd9030f3dc0d1d9d9fab5323ef
Marcel Hoffs <marcelhoffs@gmail.com>
2021-12-27 11:44:02 +0100

    commit

--- scripts/del_logs.sh
+++ scripts/del_logs.sh
@@ -31,59 +31,50 @@ delete_web_server_logs()
   if [ "$test" = 'Y' ]; then
     echo "$cmd1_arg"
     #echo "$cmd2_arg"
-    
     echo ''
   else
     rm -r $cmd1_arg
     #rm -r $cmd2_arg
   fi
 }
-#
-#delete_app_server_logs()
-#{
-#  test=$1
-#  arg1=$2
-#
-#  cmd1_arg="${arg1:?}/LOGS/*"
-#  cmd2_arg="${arg1:?}/ULOG.*"
-#
-#  if [ "$test" = 'Y' ]; then
-#    echo -e "Application server logs"
-#    echo -e "-----------------------"
-#
-#    # Paths
-#    echo "$cmd1_arg"
-#    echo "$cmd2_arg"
-#
-#    echo ''
-#  else
-#    rm -r $cmd1_arg
-#    rm -r $cmd2_arg
-#  fi
-#}
-#
-#delete_process_scheduler_logs()
-#{
-#  test=$1
-#  arg1=$2
-#
-#  cmd1_arg="${arg1:?}/LOGS/*"
-#  cmd2_arg="${arg1:?}/ULOG.*"
-#
-#  if [ "$test" = 'Y' ]; then
-#    echo -e "Process Scheduler logs"
-#    echo -e "----------------------"
-#    
-#    # Paths
-#    echo "$cmd1_arg"
-#    echo "$cmd2_arg"
-#    
-#    echo ''
-#  else
-#    rm -r $cmd1_arg
-#    rm -r $cmd2_arg
-#  fi
-#}
+
+delete_app_server_logs()
+{
+  test=$1
+  arg1=$2
+
+  cmd1_arg="${arg1:?}/LOGS/*"
+  cmd2_arg="${arg1:?}/ULOG.*"
+
+  if [ "$test" = 'Y' ]; then
+    # Paths
+    echo "$cmd1_arg"
+    echo "$cmd2_arg"
+    echo ''
+  else
+    rm -r $cmd1_arg
+    rm -r $cmd2_arg
+  fi
+}
+
+delete_process_scheduler_logs()
+{
+  test=$1
+  arg1=$2
+
+  cmd1_arg="${arg1:?}/LOGS/*"
+  cmd2_arg="${arg1:?}/ULOG.*"
+
+  if [ "$test" = 'Y' ]; then
+    # Paths
+    echo "$cmd1_arg"
+    echo "$cmd2_arg"
+    echo ''
+  else
+    rm -r $cmd1_arg
+    rm -r $cmd2_arg
+  fi
+}
 
 # ================================================================
 # MAIN
@@ -121,12 +112,11 @@ do
   PATH_PRCS_LOG[$i]="${PS_CFG_HOME}/appserv/prcs/${arrprcs[$i]}"
 done
 
-
 # Ask to continue
 while [ "$CONTINUE" != 'Y' ] && [ "$CONTINUE" != 'N' ]; do
   # Test mode, just to show the paths that will be deleted
-  #delete_web_server_logs 'Y' "$PATH_WEB_LOG"
   
+  # Web Server
   echo -e "Web server logs"
   echo -e "---------------"
   for i in "${!PATH_WEB_LOG[@]}"
@@ -134,7 +124,21 @@ while [ "$CONTINUE" != 'Y' ] && [ "$CONTINUE" != 'N' ]; do
     delete_web_server_logs 'Y' "${PATH_WEB_LOG[$i]}"
   done
 
-  #delete_process_scheduler_logs 'Y' "$PATH_PRCS_LOG"
+  # Application Server
+  echo -e "Application server logs"
+  echo -e "-----------------------"
+  for i in "${!PATH_APP_LOG[@]}"
+  do
+    delete_app_server_logs 'Y' "${PATH_APP_LOG[$i]}"
+  done
+
+  # Process Scheduler
+  echo -e "Process Scheduler logs"
+  echo -e "----------------------"
+  for i in "${!PATH_PRCS_LOG[@]}"
+  do
+    delete_process_scheduler_logs 'Y' "${PATH_PRCS_LOG[$i]}"
+  done
   
   read -r -p 'Are you sure you want to continue? [Y/N]: ' CONTINUE
   CONTINUE=${CONTINUE^^}
