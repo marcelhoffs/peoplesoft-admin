e80a66aac78d67737c630da7fe341488249238e5
Marcel Hoffs <marcelhoffs@gmail.com>
2021-12-27 11:18:48 +0100

    commit

--- scripts/del_logs.sh
+++ scripts/del_logs.sh
@@ -17,73 +17,73 @@ get_domains()
   read -a arrweb <<< "$webdomains"
 }
 
-delete_web_server_logs()
-{
-  test=$1
-  arg1=$2
-
-  cmd1_arg="${arg1:?}/servers/PIA/logs/*"
-  #cmd2_arg="${arg1:?}/applications/peoplesoft/PSIGW.war/WEB-INF/"*
-
-  if [ "$test" = 'Y' ]; then
-    echo -e "Web server logs"
-    echo -e "---------------"
-
-    echo "$cmd1_arg"
-    #echo "$cmd2_arg"
-    
-    echo ''
-  else
-    rm -r $cmd1_arg
-    #rm -r $cmd2_arg
-  fi
-}
-
-delete_app_server_logs()
-{
-  test=$1
-  arg1=$2
-
-  cmd1_arg="${arg1:?}/LOGS/*"
-  cmd2_arg="${arg1:?}/ULOG.*"
-
-  if [ "$test" = 'Y' ]; then
-    echo -e "Application server logs"
-    echo -e "-----------------------"
-
-    # Paths
-    echo "$cmd1_arg"
-    echo "$cmd2_arg"
-
-    echo ''
-  else
-    rm -r $cmd1_arg
-    rm -r $cmd2_arg
-  fi
-}
-
-delete_process_scheduler_logs()
-{
-  test=$1
-  arg1=$2
-
-  cmd1_arg="${arg1:?}/LOGS/*"
-  cmd2_arg="${arg1:?}/ULOG.*"
-
-  if [ "$test" = 'Y' ]; then
-    echo -e "Process Scheduler logs"
-    echo -e "----------------------"
-    
-    # Paths
-    echo "$cmd1_arg"
-    echo "$cmd2_arg"
-    
-    echo ''
-  else
-    rm -r $cmd1_arg
-    rm -r $cmd2_arg
-  fi
-}
+#delete_web_server_logs()
+#{
+#  test=$1
+#  arg1=$2
+#
+#  cmd1_arg="${arg1:?}/servers/PIA/logs/*"
+#  #cmd2_arg="${arg1:?}/applications/peoplesoft/PSIGW.war/WEB-INF/"*
+#
+#  if [ "$test" = 'Y' ]; then
+#    echo -e "Web server logs"
+#    echo -e "---------------"
+#
+#    echo "$cmd1_arg"
+#    #echo "$cmd2_arg"
+#    
+#    echo ''
+#  else
+#    rm -r $cmd1_arg
+#    #rm -r $cmd2_arg
+#  fi
+#}
+#
+#delete_app_server_logs()
+#{
+#  test=$1
+#  arg1=$2
+#
+#  cmd1_arg="${arg1:?}/LOGS/*"
+#  cmd2_arg="${arg1:?}/ULOG.*"
+#
+#  if [ "$test" = 'Y' ]; then
+#    echo -e "Application server logs"
+#    echo -e "-----------------------"
+#
+#    # Paths
+#    echo "$cmd1_arg"
+#    echo "$cmd2_arg"
+#
+#    echo ''
+#  else
+#    rm -r $cmd1_arg
+#    rm -r $cmd2_arg
+#  fi
+#}
+#
+#delete_process_scheduler_logs()
+#{
+#  test=$1
+#  arg1=$2
+#
+#  cmd1_arg="${arg1:?}/LOGS/*"
+#  cmd2_arg="${arg1:?}/ULOG.*"
+#
+#  if [ "$test" = 'Y' ]; then
+#    echo -e "Process Scheduler logs"
+#    echo -e "----------------------"
+#    
+#    # Paths
+#    echo "$cmd1_arg"
+#    echo "$cmd2_arg"
+#    
+#    echo ''
+#  else
+#    rm -r $cmd1_arg
+#    rm -r $cmd2_arg
+#  fi
+#}
 
 # ================================================================
 # MAIN
@@ -125,24 +125,24 @@ done
 #done < <(cat domains_prcs | sed -n 1'p' | tr ',' '\n')
 
 # Ask to continue
-while [ "$CONTINUE" != 'Y' ] && [ "$CONTINUE" != 'N' ]; do
-  # Test mode, just to show the paths that will be deleted
-  #delete_web_server_logs 'Y' "$PATH_WEB_LOG"
-  
-  for i in "${PATH_APP_LOG[@]}"
-  do
-    echo "${PATH_APP_LOG[$i]}"
-  done
-
-  #delete_process_scheduler_logs 'Y' "$PATH_PRCS_LOG"
-  
-  read -r -p 'Are you sure you want to continue? [Y/N]: ' CONTINUE
-  CONTINUE=${CONTINUE^^}
-done
-
-# Delete logs
-if [ "$CONTINUE" = 'Y' ]; then
-  #delete_web_server_logs 'N' "$PATH_WEB_LOG"
-  #delete_app_server_logs 'N' "$PATH_APP_LOG"
-  #delete_process_scheduler_logs 'N' "$PATH_PRCS_LOG"
-fi
+#while [ "$CONTINUE" != 'Y' ] && [ "$CONTINUE" != 'N' ]; do
+#  # Test mode, just to show the paths that will be deleted
+#  #delete_web_server_logs 'Y' "$PATH_WEB_LOG"
+#  
+#  for i in "${PATH_APP_LOG[@]}"
+#  do
+#    echo "${PATH_APP_LOG[$i]}"
+#  done
+#
+#  #delete_process_scheduler_logs 'Y' "$PATH_PRCS_LOG"
+#  
+#  read -r -p 'Are you sure you want to continue? [Y/N]: ' CONTINUE
+#  CONTINUE=${CONTINUE^^}
+#done
+#
+## Delete logs
+#if [ "$CONTINUE" = 'Y' ]; then
+#  #delete_web_server_logs 'N' "$PATH_WEB_LOG"
+#  #delete_app_server_logs 'N' "$PATH_APP_LOG"
+#  #delete_process_scheduler_logs 'N' "$PATH_PRCS_LOG"
+#fi
